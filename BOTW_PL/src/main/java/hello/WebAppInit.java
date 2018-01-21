32f9a09eab6b7f0b6909d58f21ecc12b4f0256c6
dong.kim <dong.kim@accenturefederal.com>
2018-01-21 15:09:48 -0500

    adding config file

---
+++ BOTW_PL/src/main/java/hello/WebAppInit.java
@@ -0,0 +1,13 @@
+package hello;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = { "botw" })
+public class WebAppInit extends WebMvcConfigurerAdapter {
+
+}
